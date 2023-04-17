12a28243beb5b4f99ce380c3b3f335636f185f26
tareqzoubii <tareqzoubi4@gmail.com>
2023-04-18 01:06:44 +0300

    Version 2

--- covid_project/settings.py
+++ covid_project/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'covid_app',
     # third-party apps
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -51,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'covid_project.urls'
@@ -125,3 +127,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CORS_ORIGIN_ALLOW_ALL = True
+# CORS_ALLOW_METHODS = ('*',)
